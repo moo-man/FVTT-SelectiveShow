49348cc1a03a605b647a3fb3dd074e160fd3c4e4
Russell <russellthurman94@gmail.com>
2020-09-03 22:44:53 -0500

    Initial commit

---
+++ selectiveshow.js
@@ -0,0 +1,62 @@
+Hooks.on("ready", () => {
+    JournalEntry.prototype.show = async function (mode = "text", force = false) 
+    {
+        if (!this.owner) throw new Error("You may only request to show Journal Entries which you own.");
+        let selection = await new Promise(resolve => {
+            new SelectiveShowApp(resolve).render(true);
+        })
+
+        game.socket.emit("module.selectiveshow", {id : this.uuid, mode, force, selection})
+    }
+
+    game.socket.on("module.selectiveshow", ({id, mode, force, selection}) => {
+        if (selection.includes(game.user._id))
+            Journal._showEntry(id, mode, force)
+    })
+    
+})
+
+
+
+
+
+class SelectiveShowApp extends Application {
+
+    constructor (resolve)
+    {
+        super();
+        this.selection = resolve
+    }
+
+    static get defaultOptions() {
+        const options = super.defaultOptions;
+        options.id = "selective-show";
+        options.template = "modules/selectiveshow/selectiveshow.html"
+        options.classes.push("selective-show");
+        options.height = 300;
+        options.width = 250;
+        options.minimizable = true;
+        options.resizable = true;
+        options.title = "Selective Show"
+        return options;
+    }
+
+    getData() {
+        let data = super.getData();
+        data.users = game.users.entities.filter(u => u.active && u.data._id != game.user._id);
+        return data;
+    }
+
+
+     activateListeners(html) {
+         super.activateListeners();    
+         html.find("button").click(ev => {
+             ev.preventDefault();
+             let selector = $(ev.currentTarget).parents("form").find("select");
+             this.selection(selector.val());
+             this.close();
+
+         })
+    }
+
+}
